64d04499c72bd45d97bbfe7a361c3fb738da0ea7
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2020-07-01 22:57:43 +0200

    Almost there...

--- run.bash
+++ run.bash
@@ -1,1 +1,2 @@
 tis-kernel -val -val-profile interpreter -no-deps-show-progress -no-val-print -info-json-results test1.json -tis-config-load tis.config -tis-config-select 1
+tis-kernel -val -val-profile interpreter -no-deps-show-progress -no-val-print -info-json-results test1.json -tis-config-load tis.config -tis-config-select 1
--- test.c
+++ test.c
@@ -1,23 +1,41 @@
 
+#include "stdlib.h"
+
+typedef unsigned char OnigUChar;
+
+typedef long long intptr_t;
+typedef intptr_t StackIndex;
+
+struct mem_struct {
+  OnigUChar *pstr ;
+  StackIndex prev_start ;
+  StackIndex prev_end ;
+};
+
+union u_union {
+  struct mem_struct mem;
+};
 
 struct _StackType {
   unsigned int type;
   int zid;
+  union u_union u;
 };
 
 typedef struct _StackType StackType;
 
-typedef long intptr_t;
-typedef intptr_t StackIndex;
-
-char t[] = "Duuuuuuuuuuuuupaaaaaaaaaaaa";
-char buf[5000];
-
 int main(void) {
-  buf[4] = &t[2];
-  StackType* stk_base = (StackType*)&buf[32];
-  StackIndex* mem_start_stk = (StackIndex*)&buf[0];
-  int i = 1;
-  int x = stk_base + *(mem_start_stk + i);
-  return x;
+  char *buf;
+  buf = malloc(5152);
+  if(buf) {
+    (*((long long *)(buf + 8))) = &("\000G\000o\000O\000o\000O\000g\000L\000e\000\000")[2];
+    (*((long long *)(buf + 24))) = &("\000G\000o\000O\000o\000O\000g\000L\000e\000\000")[10];
+    (*((long long *)(buf + 32))) = 3;
+    StackType* stk_base = (StackType*)&buf[32];
+    StackIndex* mem_start_stk = (StackIndex*)&buf[0];
+    int i = 1;
+    int x = stk_base + *(mem_start_stk + i);
+    return x;
+  }
+  return 0;
 }
